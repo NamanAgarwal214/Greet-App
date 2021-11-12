2ab5898c7f79f8a28fe1bd50393915284e09b492
NamanAgarwal <naman.ag41402@gmail.com>
2021-11-12 15:40:34 +0530

    done with the Landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,37 @@
-function App() {
+import React, {Fragment} from 'react'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import './App.css';
+import Login from './components/Layout/auth/Login';
+import Register from './components/Layout/auth/Register';
+import Home from './components/Layout/Home';
+import Navbar from './components/Layout/Navbar';
+
+
+const App = () => {
   return (
-    <>Hemlo</>
+    <Router>
+      <Fragment>
+          {/* <Navbar />
+          <Route exact path="/" component={Home} />
+        <Switch>
+					<Route path="/register" component={Register} />
+					<Route path="/login" component={Login} />
+				</Switch> */}
+        <Switch>
+          <Route exact path = '/login'>
+            <Login />
+          </Route>
+          <Route exact path = '/register'>
+            <Register />
+          </Route>
+          <Route path = '/'>
+            <Navbar />
+            <Home />
+          </Route>
+        </Switch>
+
+      </Fragment>
+    </Router>
   );
 }
 
