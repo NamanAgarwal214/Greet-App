7d432722941b18c925ecde9738fad1143206a0e6
NamanAgarwal <naman.ag41402@gmail.com>
2021-11-22 14:37:01 +0530

    small changes

--- client/src/components/home-LoggedIn/HomeLoggedIn.js
+++ client/src/components/home-LoggedIn/HomeLoggedIn.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../navbarLoggedin/Navbar'
+import Profile from '../profile/Profile'
 
 export default function HomeLoggedIn() {
   return (
     <>
       <Navbar />
+      <Profile />
     </>
   )
 }
