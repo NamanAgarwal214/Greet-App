f84e925aae78881c8fd345037a22af03be75acfa
Naman Agarwal <naman.ag41402@gmail.com>
2023-09-14 12:15:00 +0530

    cookie corrections

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -19,6 +19,8 @@ function issueJWT(res, user) {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
+    secure: true,
+    httpOnly: true,
   });
   const newUser = {
     username: user.name,
